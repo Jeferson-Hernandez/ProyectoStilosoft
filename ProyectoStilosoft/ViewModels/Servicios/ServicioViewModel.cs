8b87d2b857e8601dba91adbad6137d003a6d8977
sistemas cesge <sistemascesge@outlook.com>
2022-02-23 15:14:24 -0500

    validaciones

--- ProyectoStilosoft/ViewModels/Servicios/ServicioViewModel.cs
+++ ProyectoStilosoft/ViewModels/Servicios/ServicioViewModel.cs
@@ -17,9 +17,11 @@ namespace ProyectoStilosoft.ViewModels.Servicios
         public string Nombre { get; set; }
         [DisplayName("Duración")]
         [Required(ErrorMessage = "La duración es obligatoria")]
+        [Range(10, 999, ErrorMessage = "Ingrese un valor entre 10 min y 999 min")]
         [RegularExpression("^[0-9]*$", ErrorMessage = "Ingrese valores numéricos")]
         public int Duracion { get; set; }
         [Required(ErrorMessage = "El costo es obligatorio")]
+        [Range(5000, 1000000, ErrorMessage = "Ingrese un valor entre 5.000 y 1.000.000")]
         [RegularExpression("^[0-9]*$", ErrorMessage = "Ingrese valores numéricos")]
         public long Costo { get; set; }
         public bool Estado { get; set; }
