043e62c3e70401e8c97fb15c7d0d088231c9e477
DanielP <danielpolo210@gmail.com>
2022-02-25 17:17:11 -0500

    Usuarios-CambiarContraAdmin

--- ProyectoStilosoft/Controllers/UsuariosController.cs
+++ ProyectoStilosoft/Controllers/UsuariosController.cs
@@ -83,6 +83,19 @@ namespace Stilosoft.Controllers
                             Documento = usuarioViewModel.Documento,
                             Estado = true                            
                         };
+
+                        Usuario usuario1 = new()
+                        {
+                            UsuarioId = usuario.Id,
+                            Nombre = usuarioViewModel.Nombre,
+                            Apellido = usuarioViewModel.Apellido,
+                            Numero = usuarioViewModel.Numero,
+                            Documento = usuarioViewModel.Documento,
+                            Rol = "Cliente",
+                            Estado = true
+                           
+                        };
+                        await _usuarioService.GuardarUsuario(usuario1);
                         await _clienteService.GuardarCliente(cliente);
                         TempData["Accion"] = "Registrar";
                         TempData["Mensaje"] = "Usuario registrado correctamente";
@@ -417,7 +430,7 @@ namespace Stilosoft.Controllers
             if (ModelState.IsValid)
             {
                 //buscamos el usuario
-                var usuario = await _userManager.FindByEmailAsync(resetearPasswordDto.Email);
+                var usuario = await _userManager.FindByEmailAsync(resetearPasswordDto.Email);              
 
                 if (usuario != null)
                 {
@@ -440,16 +453,44 @@ namespace Stilosoft.Controllers
             return View(resetearPasswordDto);
         }
 
+        /// Debe permitir al admin cambiar las contraseñas de los usuarios
+        [HttpGet]
+        public IActionResult ResetPassword(string id, IdentityUser identityUser)
+        {                                 
+            return View();                
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(string id, CambiarPasswordDto cambiarPassWord)
+        {
+            if (ModelState.IsValid)
+            {
+                //buscamos el usuario
+                var usuario = await _userManager.FindByIdAsync(id);
+                var token = await _userManager.GeneratePasswordResetTokenAsync(usuario);            
+             
+                if (usuario != null)
+                {
+                    //se resetea el password
+                    var result = await _userManager.ResetPasswordAsync(usuario, token, cambiarPassWord.Password);
+                   
+                }
+                return RedirectToAction("index");
+            }
+            return RedirectToAction("index");
+        }
+
+        private bool ClienteExists(string id)
+        {
+            return _context.clientes.Any(e => e.ClienteId == id);
+        }
+
         [HttpGet]
         public async Task<IActionResult> CerrarSesion()
         {
             await _signInManager.SignOutAsync();
             //_httpContextAccessor.HttpContext.Session.Clear();
             return RedirectToAction("login", "Usuarios");
-        }
-        private bool ClienteExists(string id)
-        {
-            return _context.clientes.Any(e => e.ClienteId == id);
         }
     }
 }
