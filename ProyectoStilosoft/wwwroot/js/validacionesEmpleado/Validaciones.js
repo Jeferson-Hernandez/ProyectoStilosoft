70262428ca759e992b8b2d42f3a640f394feedd7
Jeferson-Hernandez <jefry_2558@hotmail.com>
2022-03-02 00:50:10 -0500

    empleado validaciones

---
+++ ProyectoStilosoft/wwwroot/js/validacionesEmpleado/Validaciones.js
@@ -0,0 +1,10 @@
+﻿const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+    nombre: /^[a-zA-ZÀ-ÿ\s]{3,30}$/,
+    apellido: /^[a-zA-ZÀ-ÿ\s]{3,30}$/,
+    documento: /^\d{6,15}$/,
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    password: /^.{6,30}$/
+}
