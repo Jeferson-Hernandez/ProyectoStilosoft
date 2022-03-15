7bdf8829a6cf2d0873d58d4a586d5eab83a48762
Guillermo <gr2@misena.edu.co>
2022-03-14 20:10:29 -0500

    Informes

---
+++ ProyectoStilosoft/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoStilosoft.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult gpastel()
+        {
+            return View();
+        }
+
+        public IActionResult barras()
+        {
+            return View();
+        }
+    }
+}
