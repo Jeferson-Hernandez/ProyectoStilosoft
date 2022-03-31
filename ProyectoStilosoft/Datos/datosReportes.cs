0443795ace24c4459faf056fdeb5ce62f17d037c
Guillermo <gr2@misena.edu.co>
2022-03-31 18:13:17 -0500

    informes

--- ProyectoStilosoft/Datos/datosReportes.cs
+++ ProyectoStilosoft/Datos/datosReportes.cs
@@ -23,7 +23,7 @@ namespace ProyectoStilosoft.Datos
 
             using (SqlConnection conexionSQL = new SqlConnection("Data Source = DESKTOP-STNJGIJ; Initial catalog = Stilosoft; Integrated Security = True"))
             {
-                string query = "sp_servicios";
+                string query = "sp_pruebauno";
 
                 SqlCommand cmd = new SqlCommand(query, conexionSQL);
                 cmd.CommandType = CommandType.StoredProcedure;
--- ProyectoStilosoft/wwwroot/js/pastel.js
+++ ProyectoStilosoft/wwwroot/js/pastel.js
@@ -1,66 +1,92 @@
-﻿$(document).ready(function () {
-    $.ajax({
-        url: '@Url.Action("reporteServicios", "Dashboard")',
-        type: "GET",
-        dataType: "json",
-        contentType: "application/json; charset=utf-8",
-        success: function (data) {
+﻿$.ajax({
+    url: '@Url.Action("reporteServicios", "Dashboard")',
+    type: "GET",
+    dataType: "json",
+    contentType: "application/json; charset=utf-8",
+    success: function (data) {
 
-            /*console.log(data)    */
+        /*console.log(data)    */
 
-            var arrayServicio = [];
-            var arrayCantidad = [];
+        var arrayServicio = [];
+        var arrayCantidad = [];
 
-            for (var i = 0; i < data.length; i++) {
+        for (var i = 0; i < data.length; i++) {
 
-                /*console.log(data[i].nombre)*/
+            /*console.log(data[i].nombre)*/
 
-                arrayServicio.push(data[i].servicio);
-                arrayCantidad.push(data[i].cantidad);
-                
-            }
-            DrawChart(data);
-        },
-        error: function (error) {
-            console.log(error)
+            arrayServicio.push(data[i].servicio);
+            arrayCantidad.push(data[i].cantidad);
+
         }
-    });
+        Highcharts.chart('container', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Browser market shares in January, 2018'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            accessibility: {
+                point: {
+                    valueSuffix: '%'
+                }
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                    }
+                }
+            },
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: [{
+                    name: 'Chrome',
+                    y: 61.41,
+                    sliced: true,
+                    selected: true
+                }, {
+                    name: 'Internet Explorer',
+                    y: 11.84
+                }, {
+                    name: 'Firefox',
+                    y: 10.85
+                }, {
+                    name: 'Edge',
+                    y: 4.67
+                }, {
+                    name: 'Safari',
+                    y: 4.18
+                }, {
+                    name: 'Sogou Explorer',
+                    y: 1.64
+                }, {
+                    name: 'Opera',
+                    y: 1.6
+                }, {
+                    name: 'QQ',
+                    y: 1.2
+                }, {
+                    name: 'Other',
+                    y: 2.61
+                }]
+            }]
+        });
+
+    },
+    error: function (error) {
+        console.log(error)
+    }
 });
 
 
-function DrawChart(data) {
-    Highcharts.chart('container', {
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie'
-        },
-        title: {
-            text: 'Porcentaje de servicios'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        accessibility: {
-            point: {
-                valueSuffix: '%'
-            }
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-                }
-            }
-        },
-        series: [{
-            name: 'Datos',
-            colorByPoint: true,
-            data: data
-        }]
-    });
-}
+
